ad25bb76681fdbbbe920bb837658315b79319a71
tworoose <51271998+kspaffk@users.noreply.github.com>
2019-09-15 00:41:11 -0600

    create jquery values and timer for game

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,108 @@
+function bigbangQuestion(qstn, answrs, rightA) {
+    this.question = qstn;
+    this.answers = answrs;
+    this.ansLength = answrs.length;
+    this.rightA = rightA;
 
-
-var zombieQuestion = {
-    question: "",
-    answer: [],
-
-    createHtmlContent: function(question) {
-        var contentDiv = $("<div>").addClass("content-div");
-        var questionDiv = $("div").addClass("question-div").text(question);
+    this.createHtmlContent = function() {
+        var contentDiv = $("<div class='content-div'>");
+        var questionDiv = $("<div class='question-div'>").text(this.question);
         contentDiv.append(questionDiv);
-        for(var i = 0; i < answer.length; i++) {
-            var answer = $("<div>")
-                .attr("id", "answer-" + (i + 1))
-                .text(function(i) {
-                    return answer[i];
-                });
-            contentDiv.append(answer);
+        var formDiv = $("<form>");
+        for (var i = 0; i < this.ansLength; i++) {
+            formDiv.append('<input type="radio" name="answer" value="' 
+                + i + '" id="answer-' + i + '">' + this.answers[i] + '<br>');
         }
+        contentDiv.append(formDiv);
+        return contentDiv;
+    };
+}
+
+var intervalID;
+var time = 25;
+var questionIndex = 0;
+
+var bigbangQList = [
+    new bigbangQuestion(
+        "Penny gives Sheldon and Leonard a toy. Sheldon breaks the toy. What was the toy?",
+        [
+            "Batman's utility belt",
+            "Star Trek transporter",
+            "Game of Thrones sword",
+            "Gameboy"
+        ],
+        1
+    ),
+    new bigbangQuestion(
+        "How did Sheldon end up meeting Amy?",
+        [
+            "While working at the university",
+            "In the comic book store",
+            "Through a dating app",
+            "At a science fair"
+        ],
+        2
+    ),
+    new bigbangQuestion(
+        "Where does Leonard and Sheldon work?",
+        ["MIT", "Caltech", "Harvard", "Yale"],
+        1
+    ),
+    new bigbangQuestion(
+        "What type of scientist is Raj?",
+        [
+            "Chemist",
+            "Experimental Physicist",
+            "Microbiologist",
+            "Astrophysicist"
+        ],
+        3
+    ),
+    new bigbangQuestion(
+        "Who does Sheldon recruit to start his own Comicon?",
+        ["Carrie Fisher", "Leonard Nimoy", "Bill Gates", "James Earl Jones"],
+        3
+    )
+];
+
+$(document).ready(function() {
+    function gameInit() {
+        countdown();
+        var contentDiv = bigbangQList[questionIndex].createHtmlContent();
+        $("section").append(contentDiv);
     }
 
-}
+    function countdown() {
+        var countdownDiv = $("<div class='countdown'>");
+        $("section").append(countdownDiv);
+        intervalID = setInterval(function() {
+            $(".countdown").html("<h1>" + time + "</h1>");
+            time--;
+
+            if (time < 0) {
+                clearCountdown();
+                time = 25;
+            }
+        }, 1000);
+    }
+
+    function clearCountdown() {
+        clearInterval(intervalID);
+
+        setTimeout(function() {
+            console.log("game over");
+            $("section").empty();
+            if (questionIndex < bigbangQList.length - 1) {
+                questionIndex++;
+            } else {
+                console.log("you win");
+                return false;
+            }
+
+            gameInit();
+        }
+        , 5000);
+    }
+
+    gameInit();
+});
