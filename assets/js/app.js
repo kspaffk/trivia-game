f1d1af4a5cbbbdf9becd2f5d0b737e0b8383a126
kent <51271998+kspaffk@users.noreply.github.com>
2019-09-17 01:15:03 -0600

    add hover element and update css

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ function bigbangQuestion(qstn, answrs, rightA) {
 
 var bigbangQList = [
     new bigbangQuestion(
-        "Penny gives Sheldon and Leonard a toy. Sheldon breaks the toy. What was the toy?",
+        "Penny gave Sheldon and Leonard a toy. Sheldon broke his toy. What was the toy?",
         [
             "Batman's utility belt",
             "Star Trek transporter",
@@ -63,16 +63,18 @@ var questionIndex = 0;
 var correctResponses = 0;
 
 window.onload = function() {
+    $(".content").hide();
     var startBtn = $("<button class='start'>").text("Start");
     $(".countdown").append(startBtn);
     $(".start").on("click", gameInit);
-    $(".click-answers").on("click", chooseAnswer);
 };
 
 function gameInit() {
+    $(".content").show();
+    $(".countdown").empty();
+    $(".start").off();
     questionIndex = 0;
     correctResponses = 0;
-    $(".start").hide();
     countdown();
 }
 
@@ -80,6 +82,7 @@ function countdown() {
     // insert the question content to the screen
     var content = bigbangQList[questionIndex].createContent();
     $("section").append(content);
+    $(".answers").addClass("hover");
     $(".countdown").html("You have " + time + " seconds remaining.");
     //create the interval and time and decrease the time every second
     intervalID = setInterval(function() {
@@ -93,12 +96,12 @@ function countdown() {
         }
         // capture the answer the user clicks on
     }, 1000);
+    $(".click-answers").on("click", chooseAnswer);
 }
 
 function chooseAnswer() {
     // get the value of the item clicked on
     var elementValue = $(this).attr("value");
-    $(".answers").removeClass("click-answers");
     // see if item clicked on is right answer
     if (parseInt(elementValue) === bigbangQList[questionIndex].rightA) {
         correctResponses++;
@@ -106,7 +109,6 @@ function chooseAnswer() {
     } 
     // its not the correct answer
     else {
-        console.log("answer is not correct")
         $(".countdown").html('Wrong! "Dolphins ... might be smarter than <em>some</em> people." -Leonard');
         $("#answer-" + elementValue).addClass("answer-wrong");
     }
@@ -114,6 +116,8 @@ function chooseAnswer() {
 }
 
 function clearCountdown() {
+    $(".click-answers").off("click");
+    $(".answers").removeClass("hover");
     // clear the interval
     clearInterval(intervalID);
     time = 25;
@@ -121,34 +125,36 @@ function clearCountdown() {
     $("#answer-" + bigbangQList[questionIndex].rightA).addClass("answer-correct");
     // wait 5 secs and increase to the next question
     setTimeout(function() {
-        console.log("question answered");
         // get the next question
         if (questionIndex < bigbangQList.length - 1) {
             questionIndex++;
         // end of questions - go to the end game screen
         } else {
-            console.log("end of question list");
             endGame();
             return false;
         }
+        // remove the correct/wrong answer coloring for next question
         $(".answers").removeClass("answer-correct answer-wrong");
-        $(".answers").addClass("click-answers");
         //initialize the game again
         countdown();
-    }, 2000);
+    }, 2500);
 }
 
 function endGame() {
-    // clear out the answers divs for end game
+    // clear out the countdown statement counter put in the restart button
+    $(".countdown").empty();
+    var restartBtn = $("<button class='start'>").text("Restart");
+    $(".countdown").append(restartBtn);
+    // clear out the answers
     $(".answers").removeClass("answer-correct answer-wrong");
     $(".answers").empty();
-    // replace questions div with winning statement
+    // replace question div with winning statement
     if (correctResponses < 15) {
-        $(".question").html("You correctly answered <span class='number-correct'>" + correctResponses + "</span> questions out of 25. Would you care for a Milk Dud?");
+        $(".question").html("You correctly answered <span class='number-correct'>" + correctResponses + "</span> questions out of " + bigbangQList.length + ". Would you care for a Milk Dud?");
     } else if (correctResponses < 20) {
-        $(".question").html("You correctly answered <span class='number-correct'>" + correctResponses + "</span> questions out of 25. You can use Sheldon's parking spot!")
+        $(".question").html("You correctly answered <span class='number-correct'>" + correctResponses + "</span> questions out of " + bigbangQList.length + ". You can use Sheldon's parking spot!")
     } else {
-        $(".question").html("You correctly answered <span class='number-correct'>" + correctResponses + "</span> questions out of 25. Sheldon honors you with a napkin with the DNA of Lenord Nimoy!")
+        $(".question").html("You correctly answered <span class='number-correct'>" + correctResponses + "</span> questions out of " + bigbangQList.length + ". Sheldon honors you with a napkin with the DNA of Lenord Nimoy!")
     }
-    $(".start").show().text("Restart");
+    $(".start").on("click", gameInit);
 }
